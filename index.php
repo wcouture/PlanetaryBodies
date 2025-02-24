f31e9dbec429e991c6b2807b7933ea0c1bc102a9
Wiliam Couture <wpc21@fsu.edu>
2025-02-24 17:44:36 -0500

    Adds favicon and title

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Sketch</title>
+    <title>SolarSim</title>
+    <link rel="icon" type="image/x-icon" href="/assets/earth-png.webp">
 
     <link rel="stylesheet" type="text/css" href="style.css">
 
