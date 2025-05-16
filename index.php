7e0b90c23ed8ad3fc0f0f2e5a22cf4452c9c11de
William Couture <wcouture17@gmail.com>
2025-05-16 11:01:19 -0400

    updates path for favicon image

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>SolarSim</title>
-    <link rel="icon" type="image/x-icon" href="/assets/earth-png.webp">
+    <link rel="icon" type="image/x-icon" href="/assets/readme-images/earth-png.webp">
 
     <link rel="stylesheet" type="text/css" href="style.css">
 
